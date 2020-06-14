ea0966b02f3d32a4e2f471b1e25b27f2bd0b231c
mateusz <mateusz.basiak@gmail.com>
2020-06-14 23:51:54 +0200

    finished sage 3 on coffe machine project and complete litlle tasks

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,14 +1,4 @@
-# Write your code here
-print("Write how many cups of coffee you will need:")
-number_of_coffees = int(input("> "))
-print("For", number_of_coffees, " cups of coffee you will need:")
-water = 200
-milk = 50
-coffee = 15
-print(number_of_coffees * water, "ml of water")
-print(number_of_coffees * milk, "ml of milk")
-print(number_of_coffees * coffee, "g of coffee beans")
-
+# stage 1
 # print("Starting to make a coffee")
 # print("Grinding coffee beans")
 # print("Boiling water")
@@ -16,3 +6,43 @@ print(number_of_coffees * coffee, "g of coffee beans")
 # print("Pouring coffee into the cup")
 # print("Pouring some milk into the cup")
 # print("Coffee is ready!")
+
+# stage 2
+
+# # Write your code here
+# print("Write how many cups of coffee you will need:")
+# number_of_coffees = int(input("> "))
+# print("For", number_of_coffees, " cups of coffee you will need:")
+# water = 200
+# milk = 50
+# coffee = 15
+# print(number_of_coffees * water, "ml of water")
+# print(number_of_coffees * milk, "ml of milk")
+# print(number_of_coffees * coffee, "g of coffee beans")
+
+# stage 3
+print("Write how many ml of water the coffee machine has:")
+water_supply = int(input("> "))
+print("Write how many ml of milk the coffee machine has:")
+milk_supply = int(input("> "))
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee_supply = int(input("> "))
+print("Write how many cups of coffee you will need:")
+number_of_coffees = int(input("> "))
+
+water = 200
+milk = 50
+coffee = 15
+# how many coffes can make with supply provided
+n_by_water = water_supply // water
+n_by_milk = milk_supply // milk
+n_by_coffee = coffee_supply // coffee
+# minimum value is amount of coffes coffee machine can make
+n_total = min(n_by_milk, n_by_water, n_by_coffee)
+
+if number_of_coffees == n_total:
+    print("Yes, I can make that amount of coffee")
+elif number_of_coffees > n_total:
+    print("No, I can make only", n_total, "cups of coffee")
+elif number_of_coffees < n_total:
+    print("Yes, I can make that amount of coffee (and even", n_total - number_of_coffees, "more than that)")
---
+++ Problems/Calculator/task.py
@@ -0,0 +1,21 @@
+# put your python code here
+first_number = float(input())
+second_number = float(input())
+operator = input()
+if second_number == 0.0:
+    print("Division by 0!")
+else:
+    if operator == "+":
+        print(first_number + second_number)
+    elif operator == "-":
+        print(first_number - second_number)
+    elif operator == "/":
+        print(first_number / second_number)
+    elif operator == "*":
+        print(first_number * second_number)
+    elif operator == "mod":
+        print(first_number % second_number)
+    elif operator == "pow":
+        print((first_number) ** second_number)
+    elif operator == "div":
+        print(first_number // second_number)
